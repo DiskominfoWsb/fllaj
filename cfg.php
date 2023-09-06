bef3df7a14d7fd3cb93ad3aa40792d4f4e077615
Diskominfo <diskominfo.wonosobokab@gmail.com>
2023-09-06 15:14:05 +0700

    perbaikan gitignore

---
+++ cfg.php
@@ -0,0 +1,20 @@
+<?php
+//DATABASE WEB
+
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+
+define('CAPTCHAKU', FALSE);
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'fllajwonosobo');
+define('DB_PASS', 'fllajwsb$2021*');
+define('DB_NAME', 'fllaj');
+
+define('APP_ID', '43tfg');	
+//TEMPLATE
+define('THEME', 'adminlte');
+define('LOGIN_THEME', 'new');
+define('FRONT_THEME', 'stack');
+
+?>
